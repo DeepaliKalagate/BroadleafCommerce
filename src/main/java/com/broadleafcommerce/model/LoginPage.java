99835dfa0dd76b0924762a1cda855df4e7ca15c0
Deepali Vasant Kalagate <deepalikalagate@Deepalis-MacBook-Pro.local>
2020-03-06 17:19:05 +0530

    Added code and script for Register page and Login page for broadleaf website

--- src/main/java/com/broadleafcommerce/model/LoginPage.java
+++ src/main/java/com/broadleafcommerce/model/LoginPage.java
@@ -1,6 +1,44 @@
 package com.broadleafcommerce.model;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage
 {
+    @FindBy(xpath = "//a[contains(text(),'Login')]")
+    private WebElement login;
 
+    @FindBy(id="username")
+    private WebElement email;
+
+    @FindBy(id = "password")
+    private WebElement password;
+
+    @FindBy(xpath = "//button[contains(text(),'Login')]")
+    private WebElement clickLogin;
+
+    public LoginPage(WebDriver driver)
+    {
+        PageFactory.initElements(driver,this);
+    }
+
+    public void setLogin()
+    {
+        login.click();
+    }
+    public void setEmail(String username)
+    {
+        email.sendKeys(username);
+    }
+    public void setPassword(String password1)
+    {
+        password.sendKeys(password1);
+    }
+
+    public void setClickLogin()
+    {
+        clickLogin.click();
+    }
 }
--- src/test/java/com/broadleafcommerce/script/BroadLeafCommerceTest.java
+++ src/test/java/com/broadleafcommerce/script/BroadLeafCommerceTest.java
@@ -1,13 +1,23 @@
 package com.broadleafcommerce.script;
 
 import com.broadleafcommerce.base.Browser;
+import com.broadleafcommerce.model.RegisterPage;
 import org.testng.annotations.Test;
 
 public class BroadLeafCommerceTest extends Browser
 {
     @Test
-    public void givenURL_WhenValid_ThenShouldLaunchSuccessfully()
+    public void givenURL_WhenValid_ThenShouldLaunchSuccessfully() throws InterruptedException
     {
-        setUp();
+        RegisterPage registerPage=new RegisterPage(driver);
+        registerPage.setLogin();
+        Thread.sleep(500);
+        registerPage.setEmail("dipakalagate1991@gmail.com");
+        Thread.sleep(500);
+        registerPage.setFirstName("Deepali");
+        registerPage.setLastName("Patil");
+        registerPage.setPassword("jaisai12");
+        registerPage.setConfirmPassword("jaisai12");
+        registerPage.setClickRegister();
     }
 }
