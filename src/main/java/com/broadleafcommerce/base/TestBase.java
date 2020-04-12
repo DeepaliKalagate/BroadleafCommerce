3ee1003ebca618c29220d6a989b5a42b216139d2
DeepaliKalagate <dipakalagate1991@gmail.com>
2020-04-12 22:20:14 +0530

    Refactored code to remove unwanted code

--- src/main/java/com/broadleafcommerce/base/TestBase.java
+++ src/main/java/com/broadleafcommerce/base/TestBase.java
@@ -2,11 +2,11 @@ package com.broadleafcommerce.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
